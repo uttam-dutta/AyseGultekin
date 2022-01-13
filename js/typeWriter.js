3a39f4a81e7de1812bd23ec85d95812d5821d617
Uttam Dutta <63539946+uttam-dutta@users.noreply.github.com>
2022-01-13 17:23:57 +0530

    Add files via upload

---
+++ js/typeWriter.js
@@ -0,0 +1,36 @@
+
+let i = 0;
+let text1 = "Merhaba Ayşe Hanım.";
+let text2 = "Bugün benim için en büyük gün 🌻 Doğum günün kutlu olsun Ayşem."
+let speed = 100;
+
+function typeWriter(text, para){
+	if(ok == 2){
+		clearInterval(typeInterval);
+	}
+	if(i < text.length){
+		document.getElementById(para).innerHTML += text.charAt(i);
+		i++;
+		speed = Math.random() * 50 + 100;
+	}
+	else{
+		if(ok == 0){
+			i = 0;
+		}
+		ok += 1;
+	}
+}
+
+var typeInterval;
+
+//window.onload = function() {
+//	window.onload = function(){};
+   	typeInterval = setInterval(function(){
+		if(ok == 0){
+			typeWriter(text1, "txt1");
+		}
+		else if(ok == 1){
+			typeWriter(text2, "txt2");
+		}
+	}, 100);
+//};
